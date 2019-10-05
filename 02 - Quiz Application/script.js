2017d5d3157db0eb2bf4838ffe64d4c08e7da81d
UltimateCreator3000 <proartcode@gmail.com>
2019-10-05 23:28:06 +0300

    full logic done(scoreSum(), getAnswers(), hideAnswers(), inputHandler() - optimised)

--- 02 - Quiz Application/script.js
+++ 02 - Quiz Application/script.js
@@ -1,60 +1,115 @@
 var allQuestions = [
-{question: "Who is Prime Minister of the United Kingdom?", 
-choices: ["Boris Johnson", "Gordon Brown", "Winston Churchill", "Tony Blair"], 
-correctAnswer:0},
-{question: "In which state was George W Bush governor before becoming President of the United States?",
-choices: ["California", "Alabama", "New York", "Texas"], 
-correctAnswer:3},
-{question: "Who was the Portuguese dictator at the time of Franco?", 
-choices: ["Don Quixote", "Franco", "Salazar", "Columb"], 
+{question: "Comparative politics tends to produce certain kinds of arguments. Which of the following is the best label for those arguments?", 
+choices: ["a. Basic Descriptions", 
+          "b. Rationalizations", 
+          "c. Explanations", 
+          "d. Tautologies"], 
 correctAnswer:2},
+
+{question: "Which of the following could be a good comparative politics explanation of the revolutions of the Arab Spring?",
+choices: ["a. Every country naturally turns to democracy: it's just a matter of when.", 
+          "b. The revolutions were products of revolutionary processes.",
+          "c. The Arab Spring happened because the relevant countries have culture.", 
+          "d. Important groups felt discontent, the capacity to organize increased, and the relevant states were unable to repress dissenters."
+], 
+correctAnswer:3},
+
+{question: "Which of the following is the best social scientific question?", 
+choices: ["a. Why is development better than underdevelopment for countries in Africa?", 
+          "b. Why should we listen when some people in Africa complain about their lack of rights?",
+          "c. Why is capitalism the morally preferable economic system?", 
+          "d. Why have some sub-Saharan African countries been so much better off than others in recent years?"
+], 
+correctAnswer:3},
 {question: "What is the most spoken language in Belgium?", 
 choices: ["Dutch", "German", "French", "Belgian"], 
+correctAnswer:0},
+{question: "Who is Prime Minister of the United Kingdom?", 
+choices: ["Boris Johnson", /*"Gordon Brown", */"Winston Churchill", "Tony Blair"], 
+correctAnswer:0},
+{question: "Who is Prime Minister of the United Kingdom?", 
+choices: ["Boris Johnson", "Gordon Brown", /*"Winston Churchill",*/ "Tony Blair"], 
 correctAnswer:0}
 ];
+
 var options = document.getElementsByTagName("span");
+var answers = document.getElementsByClassName("answer");
 var question = document.getElementById("question");
 var submit = document.querySelector("#submit");
+var scorePage = document.getElementById("score");
+scorePage.style.display = 'none';
+var form = document.querySelector('form');
 
-const length = options.length; // actually you must count choices
-var answers = [];
+
+const answersLength = answers.length;
+const questLength = allQuestions.length;
 var nextQuestion = 0;
+var userAnswers = [];
+let correctAnswers = [];
+for (let i = 0; i < questLength; i++) {
+	correctAnswers.push(allQuestions[i].correctAnswer);
+}
+console.log(correctAnswers);
 
 
-/*init();
+init();
 
-function init(){
-	nextQuestion(1);
-	/*answer();
-	reset();*/
+function init() {
+	hideAnswers();
+	setQuestion(nextQuestion);
+}
+
+function inputHandler() {
+	nextQuestion++;
+	if (nextQuestion < questLength) {
+		getAnswers();
+		hideAnswers();
+		setQuestion(nextQuestion);
+	} else {
+		getAnswers();
+		scorePage.textContent = scoreSum(correctAnswers, userAnswers);
+		form.style.display = 'none';
+		scorePage.style.display = "block";			
+	}
+};
+
+
+function setQuestion(num) {
+		// updating the question
+		question.textContent = allQuestions[num].question;
+
+		let choicesLength = allQuestions[num].choices.length;
+		console.log("choices : ", choicesLength);
+		for (let i = 0; i < choicesLength; i++) {
+			// as many spans as available choices 
+			answers[i].style.visibility = 'visible';
+			// filling the spans with choices
+			options[i].textContent = allQuestions[num].choices[i];
+		}}
 
 	
+// getting and saving the answer of the user in a global var	
+function getAnswers() {
+	let data = document.querySelector('input[name="input"]:checked').value;
+	userAnswers.push(data);
+	console.log(userAnswers);
+}
 
-	setQuestion(nextQuestion);
 
-
-
-	function inputHandler() {
-		nextQuestion++;
-		if (nextQuestion  < 5) {
-			let correctAnswer = allQuestions[nextQuestion].correctAnswer;
-
-			var test = document.querySelector('input[name="input"]:checked').value;
-			answers.push(test);
-			console.log(answers);
-
-			setQuestion(nextQuestion);
-
-			console.log(nextQuestion);
-		} else {
-			console.log("you reached the limit of questions");
-		}
-
-	};
-
-	function setQuestion(num) {
-		question.textContent = allQuestions[nextQuestion].question;
-		for (let i = 0; i < length; i++) {
-			options[i].textContent = allQuestions[nextQuestion].choices[i];
+// count the right answers (return a string)
+function scoreSum(correctAnswers, testAnswers) {
+	let score = 0;
+	for (let i = 0; i < correctAnswers.length; i++) {
+		if (testAnswers[i] == correctAnswers[i]) {
+			score++;
 		}
 	}
+	return `You answered correctly ${score} questions`;
+}
+
+
+function hideAnswers() {
+	for(let i = 0; i < answersLength; i++) {
+		answers[i].style.visibility = 'hidden';
+	}
+}
